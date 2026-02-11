0e6a5620cb9d9ab21a44add0ea4a89f0bb219d07
Lyle de Groot <info@dgtise.com>
2026-02-11 19:40:57 +0000

    IMG-06: add minimal Dockerfile build

--- ferro-core/src/lib.rs
+++ ferro-core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod cgroups;
 pub mod container_exec;
 pub mod container_store;
 pub mod docker_auth;
+pub mod dockerfile_build;
 pub mod volume_store;
 pub mod image_manifest;
 pub mod image_config;

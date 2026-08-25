515fb4872e4275252a277ff97d8f7fa1bf87a4cd
lyle <lyledgdev@gmail.com>
2026-08-25 18:36:55 +0100

    feat(fleet): correlate commands over host control sessions

--- ferro-mgr/src/fleet/mod.rs
+++ ferro-mgr/src/fleet/mod.rs
@@ -1,5 +1,7 @@
 mod audit;
+mod control_hub;
 mod session;
 
 pub use audit::{request_digest, AuditEntry, AuditJournal, AuditResult};
+pub use control_hub::{ControlConnection, ControlHub, FleetCommand, FleetCommandResult};
 pub use session::{BrowserIdentity, FleetRole, SessionStore};
